d83656a74105671786d647fc384c985b0d696f5a
mardsco <mardsco@gmail.com>
2018-06-25 14:22:30 +0300

    bilding app v1

--- home/models/models.py
+++ home/models/models.py
@@ -10,6 +10,9 @@ class Tracks(models.Model):
     is_save = models.BooleanField(default=True)
     notes = models.CharField(max_length=250, null=True, blank=True)
 
+    def __str__(self):
+        return str(self.name)
+
 
 class Intent(models.Model):
     name = models.CharField(max_length=200)
@@ -17,15 +20,43 @@ class Intent(models.Model):
     task2 = models.CharField(max_length=50)
     task3 = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.name)
 
-class Days(models.Model):
-    name_day = models.CharField(max_length=50)
-    start_time = models.DateTimeField(default=datetime.datetime.now(), null=True, blank=True)
-    end_time = models.DateTimeField(default=datetime.datetime.now(), null=True, blank=True)
-    id_demo = models.BooleanField(default=False, blank=True)
+
+class Years(models.Model):
+    name = models.CharField(max_length=50)
 
     def __str__(self):
-        return str(self.name_day)
+        return str(self.name)
+
+
+class Months(models.Model):
+    years = models.ForeignKey(Years, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name + '  ' + self.years)
+
+
+class Weeks(models.Model):
+    months = models.ForeignKey(Months, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.name + '  ' + self.months + '  ' + self.years)
+
+
+class Days(models.Model):
+    weeks = models.ForeignKey(Weeks, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    start_time = models.DateTimeField(default=datetime.datetime.now(), null=True, blank=True)
+    end_time = models.DateTimeField(default=datetime.datetime.now(), null=True, blank=True)
+
+    # id_demo = models.BooleanField(default=False, blank=True)
+
+    def __str__(self):
+        return str(self.name + '  ' + self.weeks + '  ' + self.months + '  ' + self.years)
 
 
 class Plan(models.Model):
@@ -34,13 +65,48 @@ class Plan(models.Model):
     day = models.ForeignKey(Days, on_delete=models.CASCADE)
     amount = models.CharField(max_length=50)
 
+    def __str__(self):
+        return str(self.day + '  ' + self.tracks + '  ' + self.intent + '  ' + self.tracks + '  ' + self.amount)
+
 
 class Students(models.Model):
     tracks = models.ForeignKey(Tracks, on_delete=models.DO_NOTHING)
-    plan = models.ForeignKey(Plan, on_delete=models.DO_NOTHING)
     student = models.CharField(max_length=200)
     phone_nemper = models.CharField(max_length=10)
     choice_text = models.CharField(max_length=200, null=True, blank=True)
     number_user = models.IntegerField(default=0)
     password = models.CharField(max_length=8)
-    is_admin = models.BooleanField(default=True)
+    is_show = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.student + '  ' + self.tracks)
+
+
+class Tasks_Every_Day(models.Model):
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE)
+    is_task1 = models.BooleanField(default=False)
+    is_task2 = models.BooleanField(default=False)
+    is_task3 = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.student + '  ' + self.plan)
+
+
+class Tasks_Every_Weeks(models.Model):
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    weeks = models.ForeignKey(Weeks, on_delete=models.CASCADE)
+    present = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.student + '  ' + self.weeks)
+
+
+class Tasks_Every_Months(models.Model):
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    months = models.ForeignKey(Months, on_delete=models.CASCADE)
+    test = models.FloatField(default=0)
+
+    def __str__(self):
+        return str(self.student + '  ' + self.months)
--- home/views/views.py
+++ home/views/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
+from ..models import Students
+
+
+def index(request):
+    if is_login(request):
+        if request.session['user_type'] == 'student':
+            return render(request, 'home/index.html')
+
+    #         # return tasks_every_day(request, day_now(), request.session['member_id'])
+    #     if request.session['user_type'] == 'techer':
+    #         return students(request, day_now())
+    #
+    # last_day = Reports_all_days.objects.filter(id_demo=True, id__lt=day_now()).latest('id')
+    # tasks = Tasks_every_day.objects.filter(reports_all_days__id__lte=last_day.id)
+    # context = dict(
+    #     number_pages=tasks.aggregate(sum=Sum('number_pages'))['sum'],
+    #     link=tasks.aggregate(sum=Sum('link'))['sum']+tasks.aggregate(sum=Sum('review'))['sum'],
+    #     last_day=last_day,
+    # )
+    # csrfContext = RequestContext(request)
+    return HttpResponseRedirect('/login')
+
+
+def login(request):
+    if "POST" == request.method:
+        try:
+            if request.POST['user_type'] == 'student':
+                m = Students.objects.get(number_user=request.POST['id_user'])
+                if m.password == request.POST.get("password", ""):
+                    request.session['user_type'] = request.POST['user_type']
+                    request.session['member_id'] = m.id
+                    request.session.set_test_cookie()
+                    return HttpResponseRedirect('/')
+            elif request.POST['user_type'] == 'techer':
+                print('techer')
+                # m = Courses.objects.get(numper_user=request.POST['id_user'])
+                # if m.password == request.POST.get("password", ""):
+                #     request.session['user_type'] = request.POST['user_type']
+                #     request.session['member_id'] = m.id
+                #     request.session.set_test_cookie()
+                #     return HttpResponseRedirect('/')
+        except: #(Courses.DoesNotExist, Students.DoesNotExist):
+            erorr_user = 'أسم المستخدم أو كلمة المرور غير صحيحة'
+            context = {'erorr_user': erorr_user}
+            return render(request, 'login.html', context)
+    # if request.session.test_cookie_worked():
+    #     return students(request, request.session['member_id'])
+    return render(request, 'login.html')
+
+def is_login(request):
+    return request.session.test_cookie_worked() and "member_id" in request.session
+
